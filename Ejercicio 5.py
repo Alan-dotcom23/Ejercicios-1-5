f6a487b604f4076e50d0ce134d95b2c859b36aaa
Alan-dotcom23 <bastianmv123@gmail.com>
2025-05-11 22:59:47 -0400

    Perfeccionando el ejercicio 5

--- Ejercicio 5.py
+++ Ejercicio 5.py
@@ -1,30 +1,32 @@
 Lista_Libros = [
-    "Tokio blues (Norwegian Wood) - Haruki Murakami",
-    "Cien años de soledad - Gabriel García Márquez",
-    "El marciano - Andy Weir",
-    "La carretera - Cormac McCarthy",
-    "El nombre del viento - Patrick Rothfuss",
+    "Tokio blues (Norwegian Wood)",
+    "Cien años de soledad",
+    "El marciano",
+    "La carretera",
+    "El nombre del viento",
 ]
 
 print("¡Bienvenido a la biblioteca digital!")
 
 def mostrar_menu():
     print("===== Menú =====")
-    print("Seleccione la opción que desea realizar:")
     print("0. Salir")
     print("1. Ver todos los libros")
     print("2. Buscar libro")
     print("3. Agregar libro")
     print("4. Eliminar libro")
-    
-while True:
-    mostrar_menu()
-    opcion = input("Selecciona una opción (0-4): ")
+    print("5. Mostrar menú")
 
+mostrar_menu()
+print("Selecciona una opción (0-5): ")
+while True:
+    opcion = input()
+    
     if opcion == "1":
         print("===== Lista de libros disponibles: =====")
         for libro in Lista_Libros:
             print(libro)
+        print("===== Para ver el menu presione 5 ======")
 
     elif opcion == "2":
         nombre = input("Introduce el nombre del libro que deseas buscar: ").strip()
@@ -53,13 +55,16 @@ while True:
             print("El libro ha sido eliminado de la biblioteca.")
         else:
             print("El libro no está disponible en la biblioteca.")
+    
+    elif opcion == "5":
+        mostrar_menu()
 
     elif opcion == "0":
         print("Saliendo del programa. ¡Hasta luego!")
         break
 
     else:
-        print("Opción no válida, por favor elija una opción del 0 al 4.")
+        print("Opción no válida, por favor elija una opción del 0 al 5.")
 
 
 
